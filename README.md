714302d78d908954c62d074bd2460d8fd407b5f5
marlonramirez <marlonramirez@outlook.com>
2015-11-06 08:35:41 -0500

    correction in the readme

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 pgAudit
 =======
-Sistema de auditoria para tablas postgreSQL basada en triggers.
+Sistema base de auditoria para tablas postgreSQL.
 
 Instalación
-------------
-Basta con ejecutar el script pgaudit.sql dentro de la base de datos a auditar, este proceso generara un esquema y dos funciones.
+-----------
+Se debe ejecutar el script **pgaudit.sql** dentro de la base de datos a auditar, este proceso generara el esquema pgaudit y las funciones table y trail.
 
 uso
 ---
-Para auditar una tabla se hace uso de la función **pgaudit.table(schema, table)**, la cual se encarga de crear la tabla de auditoria *pgaudit.schema$table* dentro del esquema pgaudit.
+Para auditar una tabla se hace uso de la función *pgaudit.table(schema, table)*, la cual se encarga de crear la tabla de auditoria *pgaudit.schema$table* dentro del esquema pgaudit.
 
 | Nombre        | Descripción                                                                                                       |
 |---------------|-------------------------------------------------------------------------------------------------------------------|
@@ -20,15 +20,17 @@ Para auditar una tabla se hace uso de la función **pgaudit.table(schema, table)
 | old           | Registro anterior a la modificación realizada, si la modificación fue de tipo I este registro se encuentra vacio  |
 | new           | Registro posterior a la modificación realizada, si la modificación fue de tipo D este registro se encuentra vacio |
 
-Si se realiza una operación DDL el sistema registra automaticamente los campos: id, register_date, user_db, comando, old y new. Si se desea realizar auditoria directa sobre la aplicación se debe hacer uso del campo log_id para referenciar la tabla que se encargara de mantener la información de ingreso del usuario al sistema, el registro de esta información se debe realizar implementando la función **pgaudit.trail(idLog)** dentro de la misma aplicación.
+Si se realiza una operación DML sobre la tabla auditada el sistema registra automaticamente dentro de la tabla de auditoria los campos: id, register_date, user_db, comando, old y new. Si se desea realizar auditoria directa sobre la aplicación se debe hacer uso del campo log_id para referenciar la tabla que se encargara de mantener la información de ingreso del usuario al sistema, el registro de esta información se debe realizar implementando la función *pgaudit.trail(idLog)* dentro de la misma aplicación.
 
 ```php
+<?php
+$db = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser, $dbpass);
 $db->exec("SELECT pgaudit.trail($_SESSION['log'])");
 ```
 
 Las operaciones soportadas por pgAudit son: INSERT, UPDATE y DELETE, cada una de estas operaciones crea un registro en la tabla de auditoria.
 
-Un registro se puede recuperar facilmente desde la tabla de auditoria con tan solo usar los campos old y new.
+Un registro se puede recuperar facilmente desde la tabla de auditoria con tan solo usar los campos *old* y *new*, estos almacenan el registro que tenia la tabla antes y despues de realizar la operación DML, respectivamente.
 
 ```sql
 INSERT INTO public.usuario
@@ -37,7 +39,7 @@ FROM pgaudit.public$usuario
 WHERE id = 1;
 ```
 
-El manejo de registros como tipo de dato es tan versatil y poderoso que se pueden realizar consultas gracias a ellos.
+El manejo de registros como tipo de dato es tan versatil y poderoso que no solo puede restablecer una tabla del sistema, si no que puede realizar cualquier otro tipo de operación como consultas.
 
 ```sql
 SELECT (new).id
--- pgaudit.sql
+++ pgaudit.sql
@@ -1,91 +1,87 @@
 ﻿CREATE SCHEMA IF NOT EXISTS pgaudit;
 
---@install
 CREATE OR REPLACE FUNCTION pgaudit.trail(integer) RETURNS VOID
-LANGUAGE plpgsql AS $install$
+LANGUAGE plpgsql AS $trail_session$
 DECLARE
-	session_value ALIAS FOR $1;
+    session_value ALIAS FOR $1;
 BEGIN
-	PERFORM relname
-	FROM pg_class
-	WHERE relname = 'tbl_session'
-	AND CASE WHEN has_schema_privilege(relnamespace, 'USAGE')
-		THEN pg_table_is_visible(oid) ELSE false END;
-
-	IF not found THEN
-		CREATE TEMPORARY TABLE tbl_session (name TEXT, value TEXT);
-	ELSE
-		DELETE FROM tbl_session WHERE name = 'log_id';
-	END IF;
-
-	INSERT INTO tbl_session VALUES ('log_id', session_value);
+    PERFORM relname
+    FROM pg_class
+    WHERE relname = 'tbl_session'
+    AND CASE WHEN has_schema_privilege(relnamespace, 'USAGE')
+        THEN pg_table_is_visible(oid) ELSE false END;
+    --Crea la tabla de session
+    IF not found THEN
+        CREATE TEMPORARY TABLE tbl_session (name TEXT, value TEXT);
+    ELSE
+        DELETE FROM tbl_session WHERE name = 'log_id';
+    END IF;
+    --Ingresa el valor de log_id
+    INSERT INTO tbl_session VALUES ('log_id', session_value);
 END
-$install$;
+$trail_session$;
 
---@table
 CREATE OR REPLACE FUNCTION pgaudit.table(name, name) RETURNS VARCHAR
 LANGUAGE plpgsql AS $audit_table$
 DECLARE
-	trigger_auditor TEXT;
-	table_name ALIAS FOR $2;
-	table_origin TEXT;
-	schema_audit TEXT;
-	table_log TEXT;
+    trigger_auditor TEXT;
+    table_name ALIAS FOR $2;
+    table_origin TEXT;
+    schema_audit TEXT;
+    table_log TEXT;
 BEGIN
-	table_origin := $1||'.'||$2;
-	schema_audit := 'pgaudit';
-	table_log := schema_audit||'.'||$1||'$'||$2;
+    table_origin := $1||'.'||$2;
+    schema_audit := 'pgaudit';
+    table_log := schema_audit||'.'||$1||'$'||$2;
+    --Crea la tabla de auditoria
+    EXECUTE 'CREATE TABLE IF NOT EXISTS '||table_log||' ('||
+            'id             serial NOT NULL'||
+            ',register_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp'||
+            ',user_db       TEXT NOT NULL DEFAULT USER'
+            ',log_id        bigint'||
+            ',comando       char(1) NOT NULL CHECK( comando IN (''I'', ''U'', ''D''))'||
+            ',old           '||table_origin||
+            ',new           '||table_origin||
+            ',CONSTRAINT    '||schema_audit||'_'||table_name||'_pk PRIMARY KEY (id))';
+    --Crea el trigger para la tabla a auditar
+    trigger_auditor := $FUNCTION$
+        CREATE OR REPLACE FUNCTION TG_TABLE_NAME_audit() RETURNS TRIGGER STRICT LANGUAGE plpgsql
+        AS $PROC$
+        DECLARE
+            log_id BIGINT;
+        BEGIN
+            PERFORM relname
+            FROM pg_class
+            WHERE relname = 'tbl_session'
+            AND CASE WHEN has_schema_privilege(relnamespace, 'USAGE')
+                THEN pg_table_is_visible(oid) ELSE false END;
 
-	-- INSERTA LA TABLA PARA EL HISTÓRICO
-	EXECUTE 'CREATE TABLE IF NOT EXISTS '||table_log||' ('||
-			'id             serial NOT NULL'||
-			',register_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp'||
-			',user_db       TEXT NOT NULL DEFAULT USER'
-			',log_id        bigint'||
-			',comando       char(1) NOT NULL CHECK( comando IN (''I'', ''U'', ''D''))'||
-			',old           '||table_origin||
-			',new           '||table_origin||
-			',CONSTRAINT    '||schema_audit||'_'||table_name||'_pk PRIMARY KEY (id))';
+            IF not found THEN
+                log_id := NULL;
+            ELSE
+                log_id := (SELECT value FROM tbl_session WHERE name = 'log_id');
+            END IF;
 
-	trigger_auditor := $FUNCTION$
-		CREATE OR REPLACE FUNCTION TG_TABLE_NAME_audit() RETURNS TRIGGER STRICT LANGUAGE plpgsql
-		AS $PROC$
-		DECLARE
-			log_id BIGINT;
-		BEGIN
-			PERFORM relname
-			FROM pg_class
-			WHERE relname = 'tbl_session'
-			AND CASE WHEN has_schema_privilege(relnamespace, 'USAGE')
-				THEN pg_table_is_visible(oid) ELSE false END;
+            IF TG_OP = 'INSERT' THEN
+                INSERT INTO TG_TABLE_NAME (log_id, comando, new) VALUES (log_id, 'I', NEW);
+            ELSIF (TG_OP = 'DELETE') THEN
+                INSERT INTO TG_TABLE_NAME (log_id, comando, old) VALUES (log_id, 'D', OLD);
+            ELSIF (TG_OP = 'UPDATE') THEN
+                INSERT INTO TG_TABLE_NAME (log_id, comando, old, new) VALUES (log_id, 'U', OLD, NEW);
+            END IF;
 
-			IF not found THEN
-				log_id := NULL;
-			ELSE
-				log_id := (SELECT value FROM tbl_session WHERE name = 'log_id');
-			END IF;
+            RETURN NULL;
+        END
+        $PROC$;
+    $FUNCTION$;
 
-			IF TG_OP = 'INSERT' THEN
-				INSERT INTO TG_TABLE_NAME (log_id, comando, new) VALUES (log_id, 'I', NEW);
-			ELSIF (TG_OP = 'DELETE') THEN
-				INSERT INTO TG_TABLE_NAME (log_id, comando, old) VALUES (log_id, 'D', OLD);
-			ELSIF (TG_OP = 'UPDATE') THEN
-				INSERT INTO TG_TABLE_NAME (log_id, comando, old, new) VALUES (log_id, 'U', OLD, NEW);
-			END IF;
+    trigger_auditor := replace(trigger_auditor, 'TG_TABLE_NAME', table_log);
+    EXECUTE trigger_auditor;
+    --Crea el trigger sobre la tabla
+    EXECUTE 'DROP TRIGGER IF EXISTS audit ON '||table_origin||';CREATE TRIGGER audit '||
+        ' AFTER INSERT OR UPDATE OR DELETE ON '||table_origin||
+        ' FOR EACH ROW EXECUTE PROCEDURE '||table_log||'_audit();';
 
-			RETURN NULL;
-		END
-		$PROC$;
-	$FUNCTION$;
-
-	trigger_auditor := replace(trigger_auditor, 'TG_TABLE_NAME', table_log);
-	EXECUTE trigger_auditor;
-
-	-- CREA EL TRIGGER SOBRE LA TABLA
-	EXECUTE 'DROP TRIGGER IF EXISTS audit ON '||table_origin||';CREATE TRIGGER audit '||
-		' AFTER INSERT OR UPDATE OR DELETE ON '||table_origin||
-		' FOR EACH ROW EXECUTE PROCEDURE '||table_log||'_audit();';
-
-	RETURN table_origin||' table being audited.';
+    RETURN table_origin||' table being audited...';
 END
 $audit_table$;
